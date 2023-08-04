41442bdb046c3ef03912dd9515f61214a414986d
Leonid Grekhov <lgrekhov98@gmail.com>
2023-08-04 08:18:07 -0700

    nnm2 function
    does everything the other functions use to do

--- methods.py
+++ methods.py
@@ -33,6 +33,39 @@ def propensity_score_matching_caliper(treated_unit, control_group, k, caliper=0.
 
     return matched_control_data[propensity_diff <= caliper]
 
+
+def nnm2(medical_data, replacement, caliper, k):
+    pairs = []
+    treatment_group = medical_data[medical_data['Treatment'] == 1]
+    control_group = medical_data[medical_data['Treatment'] == 0]
+    count = 1
+
+    if replacement == 1:
+        print("Nearest Neighbor with replament")
+
+    if caliper != 0:
+        print("Nearest Neighbor with caliper")
+
+    for _, treated_unit in treatment_group.iterrows():
+        print(count)
+        count+=1
+        nn_model = NearestNeighbors(n_neighbors=k)
+        nn_model.fit(control_group[['Propensity Score']])
+        distances, indices = nn_model.kneighbors([[treated_unit['Propensity Score']]])  # Reshape to 2D array
+        if caliper != 0:
+            matched_control_data = control_group.iloc[indices[0]]
+            propensity_diff = np.abs(matched_control_data['Propensity Score'] - treated_unit['Propensity Score'])
+            nearest_neighbors = matched_control_data[propensity_diff <= caliper]
+
+        else:
+            nearest_neighbors = control_group.iloc[indices[0]]
+
+        if replacement == 1:
+            control_group = control_group[~control_group.isin(nearest_neighbors)].dropna()
+
+        pairs.append((treated_unit, nearest_neighbors))
+
+    return pairs
 """
 def nearest_neighbor_matching_without_replacement(data, treated_col='Treatment', ps_col='Propensity Score'):
     treated_data = data[data[treated_col] == 1]
