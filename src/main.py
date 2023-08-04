113397bd40298e13a608be299ffa2b53812bba1f
Leonid Grekhov <lgrekhov98@gmail.com>
2023-08-04 11:02:48 -0700

    clean up

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-import methods
+import src.methods as methods
 from scipy.spatial.distance import cdist
 
 # Set a random seed for reproducibility
@@ -29,40 +29,12 @@ def generate_dataset(total_patients, treated_patients):
     return data
 
 
-
-
 if __name__ == '__main__':
     # Generate synthetic medical dataset with additional columns and binary outcome
     # Generate the dataset
     total_patients = 100
     treated_patients = 20
     medical_data = generate_dataset(total_patients, treated_patients)
-    """   
-    treatment_group = medical_data[medical_data['Treatment'] == 1]
-    control_group = medical_data[medical_data['Treatment'] == 0]
-    print(len(treatment_group))
-    print(medical_data)
-    # Match treatment units with control units using nearest neighbors
-    k = 5  # Number of nearest neighbors to match
-    matched_pairs = []
-    count = 1
-    for _, treated_unit in treatment_group.iterrows():
-        print(count)
-        count+=1
-        
-        nearest_neighbors = methods.propensity_score_matching_caliper(treated_unit, control_group, k)
-        #print(f"the control group before drop:{control_group}")
-        #print(nearest_neighbors)
-        control_group = control_group[~control_group.isin(nearest_neighbors)].dropna()
-        
-       
-        
-        #print(f"the control group after drop:{control_group}")
-        matched_pairs.append((treated_unit, nearest_neighbors))
-        #print(matched_pairs)
-    #medical_data2d = pd.concat([pd.concat([treated, control], axis=1) for treated, control in matched_pairs])
-    
-    """
     matched_pairs = methods.nnm2(medical_data, replacement = 1, caliper = 0.02, k = 5)
     print(len(matched_pairs))
     count=1
--- src/methods.py
+++ src/methods.py
@@ -2,37 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from scipy.spatial.distance import cdist
-"""
-def nnm(treated_unit, control_group, k):
-    nn_model = NearestNeighbors(n_neighbors=k)
-    nn_model.fit(control_group[['Propensity Score']])
-    distances, indices = nn_model.kneighbors([[treated_unit['Propensity Score']]])  # Reshape to 2D array
-    return control_group.iloc[indices[0]]
-
-
-def nnmNoReplacement(treated_unit, control_group, k):
-    nn_model = NearestNeighbors(n_neighbors=k)
-    nn_model.fit(control_group[['Propensity Score']])
-    distances, indices = nn_model.kneighbors([[treated_unit['Propensity Score']]])  # Reshape to 2D array
-    #control_group = control_group[~control_group.isin(control_group.iloc[indices[0]])].dropna()
-    return control_group.iloc[indices[0]] 
-
-def propensity_score_matching_caliper(treated_unit, control_group, k, caliper=0.02):
-    # Find k-nearest neighbors in the control group for each treated individual
-    nn_model = NearestNeighbors(n_neighbors=k)
-    nn_model.fit(control_group[['Propensity Score']])
-    if treated_unit['Propensity Score'] is not None:
-        distances, indices = nn_model.kneighbors([[treated_unit['Propensity Score']]])
-    
-    #matched_indices = indices.reshape(-1)
-    matched_control_data = control_group.iloc[indices[0]]
-
-    # Apply caliper constraint
-    propensity_diff = np.abs(matched_control_data['Propensity Score'] - treated_unit['Propensity Score'])
-    #matched_data = pd.concat([treated_unit, matched_control_data[propensity_diff <= caliper]], axis=0)
-
-    return matched_control_data[propensity_diff <= caliper]
-"""
 
 def nnm2(medical_data, replacement, caliper, k):
     pairs = []
@@ -66,47 +35,3 @@ def nnm2(medical_data, replacement, caliper, k):
         pairs.append((treated_unit, nearest_neighbors))
 
     return pairs
-"""
-def nearest_neighbor_matching_without_replacement(data, treated_col='Treatment', ps_col='Propensity Score'):
-    treated_data = data[data[treated_col] == 1]
-    untreated_data = data[data[treated_col] == 0]
-    matched_data = []
-
-    while len(treated_data) > 0:
-        distances = cdist(treated_data[[ps_col]], untreated_data[[ps_col]], metric='euclidean')
-        min_distances = np.min(distances, axis=1)
-        min_index = np.argmin(distances, axis=1)
-
-        closest_index = np.argmin(min_distances)
-        treated_index = treated_data.index[closest_index]
-        untreated_index = untreated_data.index[min_index[closest_index]]
-
-        matched_data.append(pd.concat([treated_data.loc[treated_index], untreated_data.loc[untreated_index]], axis=0))
-
-        treated_data = treated_data.drop(treated_index)
-        untreated_data = untreated_data.drop(untreated_index)
-
-    matched_data = pd.concat(matched_data, ignore_index=True)
-    return matched_data
-
-def nearest_neighbor_matching_with_replacement(data, treated_col='Treatment', ps_col='Propensity Score'):
-    treated_data = data[data[treated_col] == 1]
-    untreated_data = data[data[treated_col] == 0]
-    matched_data = []
-
-    while len(treated_data) > 0:
-        distances = cdist(treated_data[[ps_col]], untreated_data[[ps_col]], metric='euclidean')
-        min_distances = np.min(distances, axis=1)
-        min_index = np.argmin(distances, axis=1)
-
-        closest_index = np.argmin(min_distances)
-        treated_index = treated_data.index[closest_index]
-        untreated_index = untreated_data.index[min_index[closest_index]]
-
-        matched_data.append(pd.concat([treated_data.loc[treated_index], untreated_data.loc[untreated_index]], axis=0))
-
-        treated_data = treated_data.drop(treated_index)
-
-    matched_data = pd.concat(matched_data, ignore_index=True)
-    return matched_data
-"""
