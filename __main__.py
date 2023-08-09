e330de8ce14b8989074080651e325aca8a9c1964
Konstantin Botnar <kosta.botnar@gmail.com>
2023-08-09 17:11:02 -0500

    Improve logging

--- __main__.py
+++ __main__.py
@@ -1,3 +1,9 @@
+import logging.config
+from src.util import FileProvider as FP
+
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
+logging.config.fileConfig(FP.get_log_config_file())
+
 from src.main import main
 
 if __name__ == '__main__':
