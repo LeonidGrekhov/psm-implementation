4e3fb3ec3e19f495152a0cb43d02ab97e42f8d36
Leonid Grekhov <lgrekhov98@gmail.com>
2023-08-15 09:26:32 -0700

    update
    adjusted to not use nearestNeighbors from sklearn

--- src/util/methods.py
+++ src/util/methods.py
@@ -10,33 +10,54 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from scipy.spatial.distance import cdist
 
-
 def nnm2(medical_data: pd.DataFrame, replacement: int, caliper: float, k: int) -> list:
     """
     Function description
+    Accepts dataframe with parameters of replacement, caliper and number of neighbors
+    returns a list of pairs
     """
+    
+    
     logger = logging.getLogger('NNM2')
     pairs = []
+    logger.debug(f'print medical data: {medical_data}')
     treatment_group = medical_data[medical_data[dd.treatment] == 1]
     control_group = medical_data[medical_data[dd.treatment] == 0]
-    count = 1
-
+    count = 0
+    
     if replacement == 1:
         print("Nearest Neighbor with replacement")
 
     if caliper != 0:
         print("Nearest Neighbor with caliper")
-
+    
     for _, treated_unit in treatment_group.iterrows():
-        logger.debug(count)
+        propensity_diff = np.abs(control_group[dd.propensity_scores] - treated_unit[dd.propensity_scores])
+        idx = propensity_diff.idxmin()
         count += 1
-        logger.debug([treated_unit[dd.propensity_scores]])
-        logger.debug(len(control_group))
+        diff = propensity_diff[idx]
+        
+        logger.debug(f'propensity_diff: {propensity_diff[idx]}')
+        match = 0
+        if caliper != 0:
+            matched_control_data = control_group.loc[idx]
+            if diff <= caliper:
+                nearest_neighbors = matched_control_data
+                match = 1
+                if replacement == 1 and match == 1:
+                    control_group = control_group.drop(control_group.loc[idx][0])    
+            else:
+                match = 0
+        else:
+            match = 1
+            nearest_neighbors = control_group.loc[idx]
+            if replacement == 1:
+                control_group = control_group.drop(control_group.loc[idx][0])
+        """
         nn_model = NearestNeighbors(n_neighbors=k)
-
         nn_model.fit(control_group[[dd.propensity_scores]])
-
         distances, indices = nn_model.kneighbors([[treated_unit[dd.propensity_scores]]])  # Reshape to 2D array
+        
         if caliper != 0:
             matched_control_data = control_group.iloc[indices[0]]
             propensity_diff = np.abs(matched_control_data[dd.propensity_scores] - treated_unit[dd.propensity_scores])
@@ -47,7 +68,8 @@ def nnm2(medical_data: pd.DataFrame, replacement: int, caliper: float, k: int) -
 
         if replacement == 1:
             control_group = control_group[~control_group.isin(nearest_neighbors)].dropna()
-
-        pairs.append((treated_unit, nearest_neighbors))
-
+        """
+        if match:
+            pairs.append((treated_unit, nearest_neighbors))
+        logger.debug(f'length of control group after execution  {len(control_group)}')
     return pairs
