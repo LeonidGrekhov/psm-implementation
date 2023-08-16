16bb858c6610da250f9459d5715ca85e1c422cd8
Leonid Grekhov <lgrekhov98@gmail.com>
2023-08-16 10:58:24 -0700

    update

--- src/util/methods.py
+++ src/util/methods.py
@@ -9,46 +9,44 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from scipy.spatial.distance import cdist
-
-def nnm2(medical_data: pd.DataFrame, replacement: int, caliper: float, k: int) -> list:
+logger = logging.getLogger(__name__)
+def nnm2(medical_data: pd.DataFrame, replacement: bool, caliper: float, k: int) -> list:
     """
-    Function description
     Accepts dataframe with parameters of replacement, caliper and number of neighbors
-    returns a list of pairs
+    returns a list of pairs (treated patient, untreated patient)
     """
-    logger = logging.getLogger('NNM2')
     pairs = []
-    logger.debug(f'print medical data: {medical_data}')
     treatment_group = medical_data[medical_data[dd.treatment] == 1]
     control_group = medical_data[medical_data[dd.treatment] == 0]
-    count = 0
     logger.debug(f'length of control group before execution  {len(control_group)}')
-    if replacement == 1:
-        print("Nearest Neighbor with replacement")
-
-    if caliper != 0:
-        print("Nearest Neighbor with caliper")
+    logger.debug(f"data size {medical_data.shape}, replacement: {replacement}, caliper: {caliper}")
     
     for _, treated_unit in treatment_group.iterrows():
-        propensity_diff = np.abs(control_group[dd.propensity_scores] - treated_unit[dd.propensity_scores])
-        idx = propensity_diff.idxmin()
-        count += 1
-        diff = propensity_diff[idx]
-        logger.debug(f'propensity_diff: {propensity_diff[idx]}')
+        control_group['DIFF'] = np.abs(control_group[dd.propensity_scores] - treated_unit[dd.propensity_scores])
+        idx = control_group['DIFF'].idxmin()
+        diff = control_group['DIFF'][idx]
         match = 0
-        if caliper != 0:
-            matched_control_data = control_group.loc[idx]
+        
+        if caliper:  
             if diff <= caliper:
-                nearest_neighbors = matched_control_data
+                filtered_control_group = control_group[control_group['DIFF'] <= caliper]
+                nearest_neighbors = filtered_control_group.sample(n=1)
                 match = 1  
+                logger.debug(f'propensity_diff: {diff}') 
+                logger.debug(f'filtered_control_group:\n {filtered_control_group}')
+                logger.debug(f'nearest_neighbors:\n {nearest_neighbors}')
             else:
-                logger.debug("diff greater than caliper")
+                logger.warning("diff greater than caliper")
                 match = 0
         else:
+            control_group = control_group.sort_values(by="DIFF")
+            filtered_k_rows = control_group.head(k)
+            nearest_neighbors = filtered_k_rows.sample(n=1)
             match = 1
-            nearest_neighbors = control_group.loc[idx]
-        if replacement == 1 and match == 1:
-            control_group = control_group.drop(control_group.loc[idx][0])   
+            logger.debug(f'filtered_k_rows:\n {filtered_k_rows}')
+            logger.debug(f'nearest_neighbors:\n {nearest_neighbors}')
+        if replacement == True and match == 1:
+            control_group = control_group.drop(nearest_neighbors.iloc[0][0])   
         if match:
             pairs.append((treated_unit, nearest_neighbors))
     logger.debug(f'length of control group after execution  {len(control_group)}')   
