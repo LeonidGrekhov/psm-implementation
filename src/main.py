02dbae5e53d73458503cc0dd81df3d304abb56a6
Leonid Grekhov <lgrekhov98@gmail.com>
2023-08-30 09:49:49 -0700

    update
    changed naming of logistic regression file to LogReg,
    changed accepted parameters to dataframe, selected columns and target for LogRegress in LogRes for ease of use,
    added multi file output for all runs,
    added accuracy, precision, recall, f1 score

--- src/main.py
+++ src/main.py
@@ -3,20 +3,17 @@ import json
 import logging
 import logging.config
 import os
-
+import datetime
 import src.util.FileProvider as FP
 import pandas as pd
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+from src.datamodel.Column import DataDictionary as dd
 
 import src.util.methods as methods
-import src.util.LogisticRegression as LogisticRegression
+import src.util.LogReg as LogReg
 import src.util.DataGenerator as DataGenerator
-from scipy.spatial.distance import cdist
+
 
 
 # Set a random seed for reproducibility
@@ -67,32 +64,73 @@ def main():
     #medical_data = LogisticRegression.LogRegress(result_df, num_params, num_params_samples, cat_params_samples) 
     #matched_pairs = methods.nnm2(medical_data, replacement=True, caliper=0.02, k_neighbors=1, method='caliper')
     samples = [(1, 0), (0, 1), (1, 1), (5, 0), (0, 5), (5, 5), (50, 0), (0, 50), (50, 50)]
-
-    for treatment in samples:
-        medical_data = LogisticRegression.LogRegress(result_df, num_params, *treatment)
-        matched_pairs_treatment = methods.nnm2(medical_data, replacement=True, caliper=0.02, k_neighbors=1, method='caliper')
-        matched_pairs = pd.concat([matched_pairs, matched_pairs_treatment])
-       
-        
+    '''
+    
+    '''
+    parameters = ['num_param_1', 'cat_param_1']
+    target = [dd.treatment]
     folder_name = "build"
-    file_name = "matched_pairs.csv"
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
+    for treatment in samples:
+        #build the column labels to be passed to logistic regression for testing purposes
+        combined_column_names = []
+        x, y = treatment 
+        if x >  0:
+            selected_numerical_column_names = result_df.columns[1:x+1]
+            combined_column_names += selected_numerical_column_names.tolist()
+        if y > 0:
+            selected_categorical_column_names = result_df.columns[num_params+1:y+51]
+            combined_column_names += selected_categorical_column_names.tolist()
+        # Combine both sets of column names
+        #create a file for each sample combination
+        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = f'match_numerical_{x}_categorical_{y}_{timestamp}.csv'
+        file_path = os.path.join(folder_name, file_name)
+        #calculate psm scores and return a new dataframe of just the sample columns with patient id and psm scores
+        data = LogReg.LogRegress(result_df, combined_column_names, target)
+        #calculate the pairs and save them to file
+        matched_pairs = methods.nnm2(data, replacement=True, caliper=0.02, k_neighbors=1, method='caliper')
+        
+        logger.debug(f'pairs: \n{matched_pairs}')
+        try:
+            with open(file_path, "w", encoding="utf-8") as f:
+                f.write("Matched Patient:\n")
+                f.write(matched_pairs.to_string(index=False) + "\n\n")
+                f.write(f"Total matched pairs: {len(matched_pairs)}\n")
+                f.flush()
+        except Exception as e:
+            logger.error(f"An error occurred while writing to the file: {e}")
 
-    file_path = os.path.join(folder_name, file_name)
-    logger.debug(f'pairs: \n{matched_pairs}')
-    try:
-        with open(file_path, "w", encoding="utf-8") as f:
-            f.write("Matched Patient:\n")
-            #f.write(matched_pairs[::2].to_string(index=False) + "\n\n")
-            f.write(matched_pairs.to_string(index=False) + "\n\n")
-            f.write("Treated Patient(s):\n")
-            #f.write(matched_pairs[1::2].to_string(index=False) + "\n\n")
+
+        plot_enable = 1
+        control_group = data[data[dd.treatment] == 0]
+        treatment_group = data[data[dd.treatment] == 1]
+
+        # Plot histograms of PSM scores for control and treatment groups
+        
+        if plot_enable:
+            logging.getLogger('matplotlib.font_manager').disabled = True
+            logging.getLogger('PIL.PngImagePlugin').disabled = True
+            #Overlap Assessment (Propensity Score Distribution)
+            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            xLabel = f'\nParameters {len(combined_column_names)} target {target}'
+            plt.figure()
+            plt.hist(control_group[dd.propensity_scores], bins=20, alpha=0.5, color='blue', label='Control Group')
+            plt.hist(treatment_group[dd.propensity_scores], bins=20, alpha=0.5, color='orange', label='Treatment Group')    
+            plt.xlabel('Propensity Score')
+            plt.ylabel('Frequency')
+            plt.legend()
+            plt.title(f'Propensity Score Distribution (linspace){xLabel}')
+            # Specify the directory where you want to save the plot
+            save_directory = "build/"
+            # Generate a timestamp
             
-            f.write(f"Total matched pairs: {len(matched_pairs)}\n")
-            f.flush()
-    except Exception as e:
-        logger.error(f"An error occurred while writing to the file: {e}")
-    
+
+            # Generate a file name with the timestamp
+            file_name = f'match_numerical_{x}_categorical_{y}_{timestamp}.png'
+
+            # Save the plot to the generated file name
+            plt.savefig(save_directory + file_name)
     logger.debug('======Finish======')
 
---
+++ src/util/LogReg.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import logging.config
+import sys
+from src.datamodel.Column import DataDictionary as dd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+import datetime
+
+logger = logging.getLogger(__name__)
+#def LogRegress(data: pd.DataFrame, num_params: int, num_params_samples: int, cat_params_samples: int) -> pd.DataFrame:
+def LogRegress(data: pd.DataFrame, parameters: list, target: list) -> pd.DataFrame:
+    """
+    Function preforms on provided data frame and returns psm scores for said data frame
+    :param data: data frame generated earlier by DataGenerator.py
+    :param parameters: number of  parameters (ie: age)
+    :param target: target
+    :return: pd.DataFrame: returns psm scores added to the data frame
+    """
+    logger.debug(f"LogRegress.  data frame size {data.shape}, parameters: {len(parameters)}, target: {target}")
+    
+    X = data[parameters]
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LogisticRegression()
+    if not X.empty:
+        model.fit(X_train, y_train) 
+
+        psm_train = model.predict_proba(X_train)[:, 1]
+        psm_test = model.predict_proba(X_test)[:, 1]
+        psm = model.predict_proba(X)[:, 1]
+        logger.debug(f'number of propensity_scores: {len(psm_test)}')
+        data[dd.propensity_scores] = psm
+        
+
+        y_pred_train = (psm_train > 0.5).astype(int)
+        y_pred_test = (psm_test > 0.5).astype(int)
+
+        # Calculate accuracy on the test set
+        accuracy_train = accuracy_score(y_train, y_pred_train)
+        accuracy_test = accuracy_score(y_test, y_pred_test)
+
+        precision_train = precision_score(y_train, y_pred_train)
+        precision_test = precision_score(y_test, y_pred_test)
+
+        recall_train = recall_score(y_train, y_pred_train)
+        recall_test = recall_score(y_test, y_pred_test)
+
+        f1_train = f1_score(y_train, y_pred_train)
+        f1_test = f1_score(y_test, y_pred_test)
+        
+
+        logger.debug(f"Training Accuracy: {accuracy_train}", )
+        logger.debug(f"Testing Accuracy: {accuracy_test}", )
+        logger.debug(f"Training Precision: {precision_train}", )
+        logger.debug(f"Testing Precision: {precision_test}", )
+        logger.debug(f"Training Recall: {recall_train}", )
+        logger.debug(f"Testing Recall: {recall_test}", )
+        logger.debug(f"Training F1 Score: {f1_train}", )
+        logger.debug(f"Testing F1 Score: {f1_test}", )
+    else:
+        logger.warning(f'Must select more than 0 columns to generate a psm score')
+        sys.exit()
+    selected = []
+    parameters = parameters
+    target = target
+    patientID = data.columns[[0]] 
+    selected += patientID.tolist() 
+    selected += parameters
+    selected += target
+    data_dict = {col: data[col] if col != dd.propensity_scores else psm for col in selected}
+
+    # Create a new DataFrame from the dictionary
+    new_df = pd.DataFrame(data_dict)
+    new_df[dd.propensity_scores] = psm
+
+    return new_df
